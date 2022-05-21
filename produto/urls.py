e12623ce54ad54b0c6fbb45d894d30c46110006d
hugomacielads <hugomacielcesar@gmail.com>
2022-05-20 21:32:24 -0300

    Teste CRUD produto

--- produto/urls.py
+++ produto/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path, include
-from .views import homepage
+from django.urls import path
+from .views import DeleteProdutoView, IndexView,\
+                   CreateProdutoView, UdpateProdutoView
 
 urlpatterns = [
-    path('', homepage),
+    path('', IndexView.as_view(), name='index'),
+    path('add/', CreateProdutoView.as_view(), name='add_produto'),
+    path('<int:pk>/update/', UdpateProdutoView.as_view(), name='upd_produto'),
+    path('<int:pk>/delete/', DeleteProdutoView.as_view(), name='del_produto')
 ]
